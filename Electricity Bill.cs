0fe8b50363e61ee649b5e2a63bd04d43ca53d196
ibtisam2022skipq <106611734+ibtisam2022skipq@users.noreply.github.com>
2022-07-04 01:20:23 +0500

    Add files via upload

---
+++ Electricity Bill.cs
@@ -0,0 +1,138 @@
+﻿Console.Write("\nPlease specify your application (Residential or Commercial) = ");
+string status = Console.ReadLine();
+Console.Write("\nEnter the consumed unit = ");
+int units = Convert.ToInt32(Console.ReadLine());
+
+if (status == "Residential")
+{
+    if (units <= 100)
+    {
+        Residential1 residential1 = new Residential1();
+        residential1.consumedUnit = units;
+        residential1.BillwithTax();
+    }
+    else if (units >100 || units < 200)
+    {
+        Residential2 residential2 = new Residential2();
+        residential2.consumedUnit = units;
+        residential2.BillwithTax();
+    }
+    else if (units >= 200 || units <= 500)
+    {
+        Residential3 residential3 = new Residential3();
+        residential3.consumedUnit = units;
+        residential3.BillwithTax();
+    }
+    else if (units > 500)
+    {
+        Residential4 residential4 = new Residential4();
+        residential4.consumedUnit = units;
+        residential4.BillwithTax();
+    }
+}
+else if (status == "Commercial")
+{
+    if (units <= 100)
+    {
+        Commercial1 commercial1 = new Commercial1();
+        commercial1.consumedUnit = units;
+        commercial1.BillwithTax();
+    }
+    else if (units > 100 || units < 200)
+    {
+        Commercial2 commercial2 = new Commercial2();
+        commercial2.consumedUnit = units;
+        commercial2.BillwithTax();
+    }
+    else if (units >= 200 || units <= 500)
+    {
+        Commercial3 commercial3 = new Commercial3();
+        commercial3.consumedUnit = units;
+        commercial3.BillwithTax();
+    }
+    else if (units > 500)
+    {
+        Commercial4 commercial4 = new Commercial4();
+        commercial4.consumedUnit = units;
+        commercial4.BillwithTax();
+    }
+
+}
+class ElectricityBill
+{
+    public int consumedUnit;
+}
+class Residential1 : ElectricityBill
+{
+    public  void BillwithTax()
+    {
+        double bill = consumedUnit * 5;
+        double billwithTax = bill * 1.13;
+        
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Residential2 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = 500 + ((consumedUnit - 100)*17);
+        double billwithTax = bill * 1.13;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Residential3 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = 500 + 1700 + ((consumedUnit - 200) * 23);
+        double billwithTax = bill * 1.13;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Residential4 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = 500 + 1700 + 6900 + ((consumedUnit - 500) * 69);
+        double billwithTax = bill * 1.13;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Commercial1 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = consumedUnit * 8;
+        double billwithTax = bill * 1.17;
+
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Commercial2 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = 800 + ((consumedUnit - 100) * 21);
+        double billwithTax = bill * 1.17;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Commercial3 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = 800 + 2100 + ((consumedUnit - 200) * 23);
+        double billwithTax = bill * 1.17;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Commercial4 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double bill = 800 + 2100 + 6900 + ((consumedUnit - 500) * 79);
+        double billwithTax = bill * 1.17;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
---
+++ Salary slip.cs
@@ -0,0 +1,163 @@
+﻿Console.WriteLine("\n----------------Salary Slip ---------------");
+Console.Write("\n\nEnter Your Designation : ");
+string designation = Console.ReadLine(); 
+
+if (designation == "Engineer" || designation == "engineer")
+{
+    Engineer employee = new Engineer();
+    employee.GrossSalary();
+
+}
+else if (designation == "Manager" || designation == "manager")
+{
+    Manager employee = new Manager();
+    employee.GrossSalary();
+
+
+}
+else if (designation == "Analyst" || designation == "analyst")
+{
+    Analayst employee = new Analayst();
+    employee.GrossSalary();
+}
+
+
+class Employee
+{
+    public int baseSalary;
+    public int fuelAllowance;
+    public int medAllowance;
+
+    public int SalarySlip()
+    {
+        baseSalary = 1500;
+        return baseSalary;
+    }
+}
+class Engineer : Employee
+{
+    public void GrossSalary()
+    {
+        Engineer engineer = new Engineer();
+        int basic = engineer.SalarySlip();
+        fuelAllowance = 100;
+        medAllowance = 500;
+        int grossSalary = basic + fuelAllowance + medAllowance;
+        Console.WriteLine("\nYour Gross Salary = $ {0}", grossSalary);
+
+    }
+}
+class Manager : Employee
+{
+    public void GrossSalary()
+    {
+        Manager manager = new Manager();
+        int basic = manager.SalarySlip();
+        fuelAllowance = 250;
+        medAllowance = 1000;
+        int grossSalary = basic + fuelAllowance + medAllowance;
+        Console.WriteLine("\nYour Gross Salary = $ {0}", grossSalary);
+    }
+}
+class Analayst : Employee
+{
+    public void GrossSalary()
+    {
+        Analayst analayst = new Analayst();
+        int basic = analayst.SalarySlip();
+        fuelAllowance = 150;
+        medAllowance = 800;
+        int grossSalary = basic + fuelAllowance + medAllowance;
+        Console.WriteLine("\nYour Gross Salary = $ {0}", grossSalary);
+    }
+}
+
+
+-------------------------------------------------------------------------------------------------------------------------
+
+Console.WriteLine("Enter the consumed unit = ");
+int units = Convert.ToInt32 (Console.ReadLine());
+ElectricityBill electricityBill = new ElectricityBill();
+electricityBill.consumedUnit = units;
+//Residential1 residential1 = new Residential1 ();
+Residential1.BillwithTax();
+
+class ElectricityBill
+{
+    public int consumedUnit;
+    public double CalculateBill(int chrg1, int chrg2, double tax)
+    {
+        double billAmount1 = (consumedUnit * chrg1) ;
+        double billAmount2 = (consumedUnit) * chrg2;
+        double billAmount3 = billAmount1 + billAmount2;
+        double billAmount = billAmount1 * tax;
+        return billAmount;
+        //Console.WriteLine("Your Electricity Bill =  {0}", billAmount);
+
+    }
+}
+class Residential1 : ElectricityBill
+{
+    public static void BillwithTax()
+    {
+        Residential1 res = new Residential1();
+        double billwithTax = res.CalculateBill(5, 10, 1.13);
+        //double billwithTax = (consumedUnit * 8) * 1.17;
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+
+------------------------------------------------------------------------------------
+Console.WriteLine("Enter the consumed unit = ");
+int units = Convert.ToInt32(Console.ReadLine());
+
+Residential1 residential1 = new Residential1 ();
+residential1.consumedUnit=units;
+residential1.BillwithTax();
+
+class ElectricityBill
+{
+    public int consumedUnit;
+    public double CalculateBill(int chrg1, int chrg2, double tax)
+    {
+        double billAmount1 = (consumedUnit * chrg1);
+        double billAmount2 = (consumedUnit-100) * chrg2;
+        double billAmount3 = billAmount1 + billAmount2;
+        double billAmount = billAmount1 * tax;
+        return billAmount;
+
+    }
+}
+class Residential1 : ElectricityBill
+{
+    public  void BillwithTax()
+    {
+        double billwithTax = this.CalculateBill(5, 0, 1.13);
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Residential2 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double billwithTax = this.CalculateBill(5, 0, 1.13);
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Residential3 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double billwithTax = this.CalculateBill(5, 0, 1.13);
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+class Residential4 : ElectricityBill
+{
+    public void BillwithTax()
+    {
+        double billwithTax = this.CalculateBill(5, 0, 1.13);
+        Console.WriteLine("Electricity Bill with Tax = {0}", billwithTax);
+    }
+}
+---------------------------------------------------------------------------------------------
